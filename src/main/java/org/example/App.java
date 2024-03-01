edcbcfc50573ca85bc5e55d3969d1ee6e14434b9
arturgola <artur.golavskiy@metropolia.fi>
2024-03-01 14:24:36 +0200

    feat: new sdk, new project

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,17 @@
+package org.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+    public static int addMe(int a, int b) {
+        return a + b;
+    }
+
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" + addMe(3, 123));
+    }
+}
